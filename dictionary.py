ede68b1f2d7e4b313b575feb69dad7620b004c93
sainaidu3 <sainaidu9675@gmail.com>
2025-10-17 10:27:30 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#use len() with a string
+mystring = "hello"
+print(len(mystring))
+#Use ien()with a list
+mylist = [1,2,3,4]
+print(len(mylist))
+#Use len()with a dictionary
+mydict = {"a":1,"b":2}
+print(len(mydict))
