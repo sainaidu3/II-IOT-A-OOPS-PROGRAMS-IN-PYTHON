f3072da19bf9176933366bb0305496efc1ae4e61
sainaidu3 <sainaidu9675@gmail.com>
2025-10-17 10:47:24 +0530

    Add files via upload

---
+++ dog cat.py
@@ -0,0 +1,13 @@
+class Dog:
+    def sound(self):
+        print("Woof!")
+class Cat:
+    def sound(self):
+        print("Meow!")
+#make objects(instances)from these classes
+my_dog = Dog()
+my_cat = Cat()
+#Call the same method on  different objects
+my_dog.sound()
+my_cat.sound()
+        
